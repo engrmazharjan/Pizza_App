aab6526fb24434640648c34ff9321af1a84dafe7
engrmazharjan <mazharj4565gmail.com>
2021-05-04 14:18:32 +0500

    production build

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,3 +5,6 @@ let mix = require("laravel-mix");
 mix
   .js("resources/js/script.js", "public/js/script.js")
   .sass("resources/scss/style.scss", "public/css/style.css");
+
+mix.minify("public/css/style.css");
+mix.minify("public/js/script.js");
